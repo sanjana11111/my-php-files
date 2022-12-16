c248cd007a943240b8a2fcca4f7f0f6e8e7eea07
sanjana <sanjana@sanjana1.com>
2022-12-16 14:43:22 +0530

    this is file

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
+//that is string
+echo "this is php file.<br>";
+//colaborator
+$fname="sanjana";
+$sname=" goldar";
 
-echo "this is php file.";
+$addname=$fname.$sname;
+echo "this is collborator example:".$addname;
+//operator
+$a=11;
+$b=15;
+//arithmetic operator(addition,subtraction,multiply.divide,modular(remainder))
+$c=$a+$b;
+echo "<br>addition:".$c;
+
+//
+
+
+
+
 
 
 
---
+++ index1.php
@@ -0,0 +1,12 @@
+<?php
+
+print_r($_REQUEST);
+
+echo "['sname']";
+echo "['fname']";
+echo "['mname']";
+echo "['email']";
+echo "['game']";
+echo "['course']";
+
+?>
