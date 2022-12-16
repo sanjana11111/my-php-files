fa6b60fdc75d8b21224ef09f5425c20ddc9eb3ee
sanjana <sanjana@sanjana1.com>
2022-12-16 17:43:06 +0530

    contain ka execution

---
+++ index2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>this is me</h1>
+
+
+    <form action="index1.php" method="get">
+       name: <input type="text" name="fname" placeholder="type name" ><br>
+        surname: <input type="text" name="sname" placeholder="type surname"><br>
+        age: <input type="text" name="age"><br>
+        <input type="submit" name="save" value="submit">
+
+</form>
+
+
+</body>
+</html>
+
+
