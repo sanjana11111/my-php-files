3147389ec5e9209277d58437fbd64812736b2df1
sanjana <sanjana@sanjana1.com>
2022-12-17 17:58:10 +0530

    about array function

---
+++ index5.php
@@ -0,0 +1,17 @@
+<?php
+//about array function
+
+//array funcations
+
+$arr1 = array("tool1","tool2","tool3");
+//add one more
+$arr1[] = "tool4";
+
+echo"<pre>";
+print_r($arr1);
+echo"</pre>";
+
+$arry2 =array(1,2,3,4,5,6,7,8,9);
+
+
+?>
