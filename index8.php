910fe6c9e0c553d5361964c2ab215ffcad278438
sanjana <sanjana@sanjana1.com>
2022-12-19 22:34:16 +0530

    pre. swich case

---
+++ index8.php
@@ -0,0 +1,20 @@
+<?php
+
+//swich statement
+$x = 6;
+
+switch ($x) {
+    case 8:
+        echo "varriable x is 8";
+        break;
+    
+    default:
+    echo "varriable x is not 8";
+        break;
+}
+
+
+
+
+
+?>
