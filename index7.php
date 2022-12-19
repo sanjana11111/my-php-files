a4e959eedaec653ca670d029c345a92e6c137c01
sanjana <sanjana@sanjana1.com>
2022-12-19 17:24:01 +0530

    loops & types

---
+++ index7.php
@@ -0,0 +1,47 @@
+<?php
+//loops - while, do-while, for, foreach loop in there
+
+//1. while loop example it's a entry control loop
+
+// $a=1;
+
+// while($a <= 10){
+
+// echo $a."<br>";
+
+// $a++;
+
+// }
+
+//  echo "its terminate now","<br>";
+
+// //print a table
+// $a=1;
+// $tab=5;
+// while($a <= 10){
+
+// echo $a*$tab."<br>";
+
+// $a++;
+
+// }
+
+// echo "its terminate now";
+
+
+//2. do-while loop example it's a exit control loop
+$a=0;
+do{
+    echo $a."<br>"; 
+    $a++;
+}while($a <= 10);
+
+
+//3. for loop example it's a entry control loop
+
+for( $a=0; $a <= 10; $a++)
+{
+    echo $a."<br>"; 
+}
+
+?>
