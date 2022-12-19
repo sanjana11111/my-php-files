b307dcb8076f94a1b5f4ce3a4ff343dfa9d4469d
sanjana <sanjana@sanjana1.com>
2022-12-19 22:22:28 +0530

    about----continue, break, count no.

--- index7.php
+++ index7.php
@@ -40,26 +40,86 @@
 
 //3. for loop example it's a entry control loop
 
+// for( $a=0; $a <= 10; $a++)
+// {
+//     echo $a."<br>"; 
+// }
+
+
+// //4. foreach loop example array ka data retrive karne ke liye foreach ka use kiya jata hain
+
+// $arr = [
+// "html" => 1100,
+// "css" => 1200,
+// "js" => 1500,
+// "php" => 1900,
+// ];
+
+// foreach( $arr as $course => $fee){
+
+// echo " </br> the fees is $course is : $fee";
+
+// }
+
+// //using break
+
+// for( $a=0; $a <= 10; $a++)
+// {
+//     if ($a == 5){
+//         break;//break se print 5 tak hi hoga or loop end hojata hain
+//     }
+//     echo $a."<br>"; 
+// }
+// echo "loop ends"
+
+//continue statment
+
+// for( $a = 0; $a <= 10 ; $a++)
+// {
+//     if($a == 5)
+//     {
+//     continue; // continue statement ko use karne se is condition me 5 pint nhi hoga........
+//     }
+//     echo "$a<br>"; 
+// }
+
+//continue statement use to print odd no. & also even no.
+
+// odd
 for( $a=0; $a <= 10; $a++)
 {
-    echo $a."<br>"; 
+    if($a%2 == 0){
+
+    continue; // continue statement ko use karne se is condition me 5 pint nhi hoga........
+    }
+    echo "$a<br>"; 
 }
+      echo"<br>...................................<br>";
+      //ya fir hum iske alava != notequle ka use kr skte h
+//even
+for( $a=0; $a <= 10; $a++)
+{
+    if($a%2 == 1){
 
-
-//4. foreach loop example it's a array 
-
-$arr = [
-"html" => 1100,
-"css" => 1200,
-"js" => 1500,
-"php" => 1900,
-];
-
-foreach( $arr as $course => $fee){
-
-echo " </br> the fees is $course is : $fee";
-
+    continue; // continue statement ko use karne se is condition me 5 pint nhi hoga........
+    }
+    echo "$a<br>"; 
 }
+echo"...................<br>";
+//count the printed numbers odd or evens anything you want
 
+$count=0;
+
+for( $a=0; $a <= 10; $a++)
+{
+    if($a%2 == 1){
+
+    continue; // 
+    }
+    $count++;
+    echo "$a<br>"; 
+}
+echo"...................<br>";
+echo $count;
 
 ?>
