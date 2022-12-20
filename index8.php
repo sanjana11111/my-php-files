82cb503b50b78a114d6efdb9cf3e4af97ebff7ee
sanjana <sanjana@sanjana1.com>
2022-12-20 07:34:51 +0530

    pre.swich

--- index8.php
+++ index8.php
@@ -1,15 +1,19 @@
 <?php
 
 //swich statement
-$x = 6;
+$x = 10;
 
 switch ($x) {
     case 8:
         echo "varriable x is 8";
         break;
+       
+        case 6:
+    echo "varriable x is not 6";
+        break;
     
     default:
-    echo "varriable x is not 8";
+    echo "varriable x is not 8 $ not 6";
         break;
 }
 
