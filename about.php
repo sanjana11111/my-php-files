15bfa070ac977ca3ae1a1bfe4b3edf0b58cfdbb7
sanjana <sanjana@sanjana1.com>
2022-12-22 17:49:06 +0530

    using include()

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+ echo include ('index9.php');
+?>
+
+<div>
+   
+    <h3>About</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut eos ex aliquid odio sequi non vel unde quaerat a, facere nulla corrupti nobis est fugiat rerum, sapiente nemo! Porro ea, magni architecto eum minima totam labore exercitationem neque. Ex eum blanditiis in, ad neque doloribus adipisci voluptate magnam molestias eos illo optio aperiam nesciunt at et esse, quam cum! Eius deserunt cum quidem quis est corrupti eligendi animi molestias quisquam! Iste eius id aut iure accusantium quas quasi repellendus aliquam! Illo veniam inventore similique, saepe quisquam distinctio hic. Eligendi, possimus.</p>
+
+</div>
+</body>
+</html>
---
+++ index9.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <style>
+        body{
+            text-align: center;
+        }
+        h1{
+            color:blueviolet;
+        }
+    </style>
+</head>
+<body>
+    <header>
+     <h1>Paragraph</h1>
+    </header>
+
+
+
+
