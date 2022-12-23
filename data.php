dd8f7ac7dbcbb12c4e1ca4c210c71b33239d01e4
sanjana <sanjana@sanjana1.com>
2022-12-23 08:50:58 +0530

    data in inside a textarea

--- data.php
+++ data.php
@@ -5,14 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data enter</title>
+    <style>
+        .tarea{
+            height:150px;
+            width:250px;
+        }
+    </style>
 </head>
 <body>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-Name:<input type="text" name="fname"><br>
-<input type="submit">
-<textarea class=""> </textarea>
+Name:<input type="text" name="fname">
+<br><br><br>
+<input type="submit" name="submit" value="submit" >
 </form>
+<br><br>
+
+<textarea class="tarea"> 
 
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
@@ -21,13 +30,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
     $name = htmlspecialchars($_REQUEST['fname']);
     if(empty ($name)){
-        echo "Name is empty";
+        
     }else{
         echo $name;
     }
 }
 
 ?>
+</textarea>
+
     
 </body>
 </html>
