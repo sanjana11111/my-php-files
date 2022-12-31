1fc679baad2f9ff78513cf770ff878f418a6f856
sanjana <sanjana@sanjana1.com>
2022-12-31 14:17:06 +0530

    rename previous php prectic file

---
+++ index10.php
@@ -0,0 +1,25 @@
+<?php
+//that is string
+echo "this is php file.<br>";
+//colaborator
+$fname="sanjana";
+$sname=" goldar";
+
+$addname=$fname.$sname;
+echo "this is collborator example:".$addname;
+//operator
+$a=11;
+$b=15;
+//arithmetic operator(addition,subtraction,multiply.divide,modular(remainder))
+$c=$a+$b;
+echo "<br>addition:".$c;
+
+//
+
+
+
+
+
+
+
+?>
