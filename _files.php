a0038316d401c3dc26697ee1b54642c021aabfb8
sanjana <sanjana@sanjana1.com>
2023-01-03 15:59:38 +0530

    readfile function

---
+++ _files.php
@@ -0,0 +1,12 @@
+<?php
+
+//file ko read kese kiya jata hain  is function
+
+// $file = readfile("textfile.txt"); //this function is also count the characters in number and print the numbers in the end of sentences.
+// echo $file;
+
+
+// readfile("textfile.txt"); //it's only print the given sentence.
+// readfile("blank.png")
+readfile("about.php")
+?>
