711d47f1471491ee9e5387c1991d28a59e7da173
sanjana <sanjana@sanjana1.com>
2023-01-03 18:14:14 +0530

    using-----fgets() & fgetc

--- _files.php
+++ _files.php
@@ -10,16 +10,51 @@
 // readfile("blank.png")
 // readfile("about.php")
 
-$fp =fopen("textfile.txt","r");//writen value resource or false
-// echo var_dump($fp);
-if(!$fp){
-die("Unable to open thi file.......");
+// $fp =fopen("textfile.txt","r");//writen value resource or false
+// // echo var_dump($fp);
+// if(!$fp){
+// die("Unable to open thi file.......");
+// }
+
+// $content = fread($fp,filesize("textfile.txt"));// peramiter>>>>>1. variable........... 2.length//writen content
+// fclose($fp);//file close
+// echo "$content";
+
+
+//Using fgets() 
+
+$fp =fopen("textfile.txt","r");
+// echo fgets($fp);//ye ek baar me sirf ek hi line print karta hain line by line
+// echo fgets($fp);
+// echo fgets($fp);
+// echo fgets($fp);
+// echo var_dump(fgets($fp));// for best method is using loop
+
+//Reading a flie line by line
+// while($a=fgets($fp)){
+//     echo $a;
+// }
+// echo "<br>End of the file has been reached";
+
+
+//Reading a file character by character
+/*
+echo fgetc($fp);//read only one character
+while($a=fgetc($fp)){
+        echo $a;
+    // break;
+    }
+    echo "<br>End of the file has been reached";
+*/
+
+//Write a program which reads the content of a file until " . " has been encountered
+while($a=fgetc($fp)){
+    echo $a;
+if($a == "."){
+    break;
 }
-
-$content = fread($fp,filesize("textfile.txt"));// peramiter>>>>>1. variable........... 2.length//writen content
-fclose($fp);//file close
-echo "$content";
-
-
+}
+fclose($fp)
+echo "<br>End of the file has been reached";
 
 ?>
