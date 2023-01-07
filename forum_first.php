80d156d9d14d7a1c57bb8902eae7248835d46f6b
sanjana <sanjana@sanjana1.com>
2023-01-07 09:04:52 +0530

    some change in navbar and first page

--- forum_first.php
+++ forum_first.php
@@ -12,8 +12,15 @@
   </head>
   <body>
     <?php include 'forum_nav.php';?>
-    <!-- Optional JavaScript; choose one of the two! -->
+    <div class="container">
+      <h2>Welcome To Our Site E-Coding</h2>
+    </div>
+   
+    <?php include 'forum_footer.php';?>
 
+
+
+     <!-- Optional JavaScript; choose one of the two! -->
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
--- forum_nav.php
+++ forum_nav.php
@@ -14,17 +14,19 @@ echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <li class="nav-item ">
         <a class="nav-link active" href="#">About</a>
       </li>
-      <div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown link
-  </a>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </li>
 
-  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
+
       <li class="nav-item">
         <a class="nav-link active" href="#" tabindex="-1" >Contact</a>
       </li>
