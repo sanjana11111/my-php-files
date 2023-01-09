03214af83eb3c096c4b8b40d52d5d8cc5bc972bf
sanjana <sanjana@sanjana1.com>
2023-01-09 18:26:42 +0530

    up to date and add img

--- forum_first.php
+++ forum_first.php
@@ -12,16 +12,57 @@
   </head>
   <body>
     <?php include 'forum_nav.php';?>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" style="margin-top:1px;">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="c2.jfif" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="c1.jfif"  class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="c5.jfif"  class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
     <div class="container mt-3" style="color:rgb(104, 62, 69);">
       <h1 class="text-center">Welcome To Our Site E-Coding</h1>
       <br>
       <h2  class="text-center mt-2 mb-4">Categories</h2>
       <div class="row">
+
+
 <!-- Use a for loop to iterate through categories. -->
 
         <div class="col-md-3">
-        <div class="card " style="width: 18rem;">
-        <img src="https://source.unsplash.com/300x200/?html,code" class="card-img-top" alt="...">
+        <div class="card my-2" style="width: 18rem;">
+        <img src="https://source.unsplash.com/400x200/?html" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -31,40 +72,10 @@
         </div>
 
         
-        <div class="col-md-3">
-        <div class="card " style="width: 18rem;">
-        <img src="https://source.unsplash.com/300x200/?html,code" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View Threads</a>
-        </div>
-      </div>
-        </div>
+       
 
-        
-        <div class="col-md-3">
-        <div class="card " style="width: 18rem;">
-        <img src="https://source.unsplash.com/300x200/?html,code" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View Threads</a>
-        </div>
-      </div>
-        </div>
 
-        
-        <div class="col-md-3">
-        <div class="card " style="width: 18rem;">
-        <img src="https://source.unsplash.com/300x200/?html,code" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View Threads</a>
-        </div>
-      </div>
-        </div>
+
 
       </div>
     </div>
--- forum_nav.php
+++ forum_nav.php
@@ -2,17 +2,17 @@
 
 echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">E-Coding</a>
+  <a class="navbar-brand" href="forum_first.php">E-Coding</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" aria-current="page" href="forum_first.php">Home</a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link active" href="#">About</a>
+        <a class="nav-link " href="forum_about.php">About</a>
       </li>
       <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -28,7 +28,7 @@ echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
 
       <li class="nav-item">
-        <a class="nav-link active" href="#" tabindex="-1" >Contact</a>
+        <a class="nav-link " href="forum_contact.php" tabindex="-3" >Contact</a>
       </li>
     </ul>
   
