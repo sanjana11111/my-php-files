1180024d588053f5c7a6d357e89000c2887f6a6d
sanjana <sanjana@sanjana1.com>
2023-01-11 09:06:07 +0530

    set navbar

--- forum_first.php
+++ forum_first.php
@@ -1,97 +1,127 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>E-Coding forums</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <?php include 'forum_nav.php';?>
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" style="margin-top:1px;">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="c2.jfif" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+    <?php include 'forum_dbconnect.php';?>
+
+    <!-- slider -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="c2.jfif" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>E-coding</h5>
+                    <p>We helping you for incresing knowledge.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="c1.jfif" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>E-coding</h5>
+                    <p>We finding you problems solution.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="c5.jfif" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>E-coding</h5>
+                    <p>We are here and always with you.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-item">
-      <img src="c1.jfif"  class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="c5.jfif"  class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+
 
     <div class="container mt-3" style="color:rgb(104, 62, 69);">
-      <h1 class="text-center">Welcome To Our Site E-Coding</h1>
-      <br>
-      <h2  class="text-center mt-2 mb-4">Categories</h2>
-      <div class="row">
+        <h1 class="text-center">Welcome To Our Site E-Coding</h1>
+        <br>
+        <h2 class="text-center mt-2 mb-2">Categories</h2>
+        <div class="row">
+
+            <!-- Fetch all  the categories -->
+            <!-- <div class="container col-mb-2"></div> -->
+            <?php
+            
+                 $sql="SELECT * FROM `categories`";
+
+               $result=mysqli_query($conn ,$sql);
+
+               while($row = mysqli_fetch_assoc($result)){
+ 
+             $cat = $row ['category_name'];
+               echo'<div class="col-md-3">
+                            <div class="card my-2" style="width: 18rem;">
+                            <img src="https://source.unsplash.com/400x200/?'.$cat.',coding" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title">'.$cat.'</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card\'s content.</p>
+                            <a href="#" class="btn btn-primary">View Threads</a>
+                        </div>
+                    </div>
+                </div>';
+
+               }
+             ?>
+
+            <!-- Use a for loop to iterate through categories. -->
+
+          
 
 
-<!-- Use a for loop to iterate through categories. -->
-
-        <div class="col-md-3">
-        <div class="card my-2" style="width: 18rem;">
-        <img src="https://source.unsplash.com/400x200/?html" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">View Threads</a>
         </div>
-      </div>
-        </div>
-
-        
-       
-
-
-
-
-      </div>
     </div>
-   
+
     <?php include 'forum_footer.php';?>
 
 
 
-     <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Optional JavaScript; choose one of the two! -->
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha383-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha383-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
-    
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-   
-  </body>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+
+</body>
+
 </html>
--- forum_nav.php
+++ forum_nav.php
@@ -2,48 +2,54 @@
 
 echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
-  <a class="navbar-brand" href="forum_first.php">E-Coding</a>
+  <a class="navbar-brand"  style="font-size:30px;" href="forum_first.php">E-Coding</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
+      <li class="nav-item" style="font-size:20px;">
         <a class="nav-link active" aria-current="page" href="forum_first.php">Home</a>
       </li>
-      <li class="nav-item ">
+      <li class="nav-item " style="font-size:20px;">
         <a class="nav-link " href="forum_about.php">About</a>
-      </li>
-      <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Categories
+      </li>';
+      ?>
+      <li class="nav-item dropdown" style="font-size:20px;">
+      <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+        Categories
       </a>
-      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      <ul class="dropdown-menu">
+
+        <li><a class="dropdown-item" href="#">Fitness</a></li>
+        <li><a class="dropdown-item" href="#">Food</a></li>
+        <li>
+          <hr class="dropdown-divider">
+        </li>
+        <li><a class="dropdown-item" href="#">Diet-Plan</a></li>
       </ul>
     </li>
 
-
-      <li class="nav-item">
+<?php
+echo '
+      <li class="nav-item" style="font-size:20px;">
         <a class="nav-link " href="forum_contact.php" tabindex="-3" >Contact</a>
       </li>
     </ul>
   
-    <form class="d-flex">
+    <form class="d-flex" style="font-size:20px;">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-primary" type="submit">Search</button>
     </form>
-    <div class="mx-2">
-    <button class="btn btn-outline-primary">Login</button>
-    <button class="btn btn-outline-primary">Signup</button>
+    <div class="mx-2" style="font-size:20px;">
+    <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+    <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</button>
     </div>
   </div>
 </div>
 </nav>';
 
-
+include 'forum_loginm.php';
+include 'forum_signupm.php';
 
 ?>
