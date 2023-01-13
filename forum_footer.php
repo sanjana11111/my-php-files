293a77c5223ce0de78f94a6ea67073e4abd1f2a5
sanjana <sanjana@sanjana1.com>
2023-01-13 07:55:51 +0530

    forum footer

--- forum_footer.php
+++ forum_footer.php
@@ -1,3 +1,4 @@
-<div class="container-fluid bg-dark text-light">
-    <p class="text-center mb-0">&#169;Copyright E-Coding forums 2023 | All rights reserved</p>
+<div class="container-fluid bg-dark text-light py-1">
+    <h5 class="text-center mb-0" style="font-weight:100;">&#169;Copyright E-Coding forums 2023 | All rights reserved</h5>
 </div>
+<!-- style="width: 130%; margin-left: -194px; -->
